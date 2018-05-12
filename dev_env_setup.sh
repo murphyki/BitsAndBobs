07d9f2b55313a71ebea2561fca0bdb6b87e37ab6
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-12 17:01:25 +0100

    Update dev_env_setup.sh

--- dev_env_setup.sh
+++ dev_env_setup.sh
@@ -1,5 +1,21 @@
 #!/bin/bash -eux
 
+# Notes:
+# 1. First, deploy a VM using VMWare Workstation, it has to be Workstation as it supports
+# nested VMs whereas, unfortunately, VirtualBox does not. We need nested VM capability
+# because minikube is deployed as a VM
+# 2. Deploy the VM from the OS ISO file, in my case this was ubuntu-16.04.4-desktop-amd64.iso
+# which i downloaded from the ubuntu downloads site. THe VM setup is super simple with Workstation
+# One thing though, check the date and time after installtion is complete, mine date and time were way off!!
+# 3. Clone this repo, change directory into the repo folder and: chmod +x dev_env_setup.sh
+# 4. Execute: ./dev_env_setup.sh
+# 5. Wait until script finishes, it'll take a while, 10/20 minutes
+# 6. You then need to log out of the VM and log back in so that docker related changes to groups take affect
+# 7. Give the VM a spin and make sure apps are working as expected
+# 8. If all goes well, take a snapshot of the VM, this will be the base snapshot we can rollback to if future
+# endeavours go pear shaped
+# 9. Go ahead and install other apps, e.g. kubernetes, IDEs, etc
+
 # Install core apps
 sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
 
