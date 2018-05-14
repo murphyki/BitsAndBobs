b36800e501f839ed837e2646a1d15028e3c95bec
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-14 13:35:15 +0100

    Update setup_docker.sh

--- dev_setup/setup_docker.sh
+++ dev_setup/setup_docker.sh
@@ -1,1 +1,13 @@
+#!/bin/bash -eux
+
+# Install docker (Community Edition)
+sudo apt-get install -y docker-ce
+
+# Configure docker group
+echo "Current user is: $USER"
+sudo usermod -aG docker $USER
+echo "Current mebers of group docker: "
+getent group docker
+
+# Note need to log out and log back in for changes to take affect
 
