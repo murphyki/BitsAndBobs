177369538f9a80b0f05a1737d6a45c35b764102a
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-14 08:17:23 +0100

    Update setup_k8s.sh

--- dev_setup/setup_k8s.sh
+++ dev_setup/setup_k8s.sh
@@ -8,7 +8,7 @@ sudo snap install kubectl --classic
 kubectl version
 
 # See: https://github.com/kubernetes/minikube/releases
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/v0.26.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
+curl -Lok minikube https://storage.googleapis.com/minikube/releases/v0.26.1/minikube-linux-amd64 && chmod +x minikube && sudo mv minikube /usr/local/bin/
 # Note:
 # 1. had to add -k to the curl command above die to certificate errors, which is not ideal...
 # 2. Downloading the minikube ISO from behind a corporate firewall may not work, so
