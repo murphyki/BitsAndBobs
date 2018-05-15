35b76bd6f434e7f297e30215efd580f7e989140f
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-15 20:14:47 +0100

    Update setup_dev_env.sh

--- dev_setup/setup_dev_env.sh
+++ dev_setup/setup_dev_env.sh
@@ -13,9 +13,6 @@ sudo add-apt-repository "deb https://download.virtualbox.org/virtualbox/debian $
 wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
 wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-
 sudo apt-get update
 sudo apt-get upgrade -y
 
