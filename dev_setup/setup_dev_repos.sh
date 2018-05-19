fb06f12d2ac3721e203fb58e47d74f63c8706866
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-19 16:47:23 +0100

    Update setup_dev_repos.sh

--- dev_setup/setup_dev_repos.sh
+++ dev_setup/setup_dev_repos.sh
@@ -4,3 +4,10 @@ WORKDIR=~/Development/repositories
 
 mkdir -p $WORKDIR
 cd $WORKDIR
+
+# Clone some sample repos for playing with kubernetes
+git clone https://github.com/silvam11/ngci-demo.git
+git clone https://github.com/kubernetes-for-developers/kfd-flask.git
+git clone https://github.com/kubernetes-for-developers/kfd-nodejs.git
+git clone https://github.com/kubernetes-for-developers/kfd-celery.git
+
