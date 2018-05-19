a32d550ce4d6eb4a0211fd2d3d7aa2b63e36af04
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-19 18:26:53 +0100

    Update setup_dev_repos.sh

--- dev_setup/setup_dev_repos.sh
+++ dev_setup/setup_dev_repos.sh
@@ -10,4 +10,4 @@ git clone https://github.com/silvam11/ngci-demo.git
 git clone https://github.com/kubernetes-for-developers/kfd-flask.git
 git clone https://github.com/kubernetes-for-developers/kfd-nodejs.git
 git clone https://github.com/kubernetes-for-developers/kfd-celery.git
-
+git clone https://github.com/spurin/masteringansible.git
