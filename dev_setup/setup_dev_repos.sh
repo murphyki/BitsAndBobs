e66c62fba07a3b79e9ce94db244b8bcc9010c2c7
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-22 14:36:14 +0100

    Update setup_dev_repos.sh

--- dev_setup/setup_dev_repos.sh
+++ dev_setup/setup_dev_repos.sh
@@ -5,9 +5,25 @@ WORKDIR=~/Development/repositories
 mkdir -p $WORKDIR
 cd $WORKDIR
 
-# Clone some sample repos for playing with kubernetes
+# Clone some sample repos for playing with kubernetes and ansible
 git clone https://github.com/silvam11/ngci-demo.git
 git clone https://github.com/kubernetes-for-developers/kfd-flask.git
 git clone https://github.com/kubernetes-for-developers/kfd-nodejs.git
-git clone https://github.com/kubernetes-for-developers/kfd-celery.git
+#git clone https://github.com/kubernetes-for-developers/kfd-celery.git
 git clone https://github.com/spurin/masteringansible.git
+
+# Use minikubes docker
+eval $(minikube docker-env)
+
+# Deploy k8s examples
+pushd .
+cd ./kfd-flask
+docker build -t kubernetes-for-developers/flask .
+kubectl run flask --image=kubernetes-for-developers/flask --port=3000 --overrides='{"spec":{"template":{"spec":{"containers":[{"name":"flask","imagePullPolicy":"Never","image":"kubernetes-for-developers/flask"}]}}}}'
+popd
+
+pushd .
+cd ./kfd-nodejs
+docker build -t kubernetes-for-developers/nodejs .
+kubectl run nodejs --image=kubernetes-for-developers/nodejs --port=3000 --overrides='{"spec":{"template":{"spec":{"containers":[{"name":"nodejs","imagePullPolicy":"Never","image":"kubernetes-for-developers/nodejs"}]}}}}'
+popd
