26fe29b5441b72f2b3ead1bac9a429b29633d43e
Kieran Murphy <kierantmurphy@gmail.com>
2018-05-22 16:41:57 +0100

    Update setup_dev_repos.sh

--- dev_setup/setup_dev_repos.sh
+++ dev_setup/setup_dev_repos.sh
@@ -2,15 +2,32 @@
 
 WORKDIR=~/Development/repositories
 
-mkdir -p $WORKDIR
+if [ ! -d "$WORKDIR" ]; then
+    mkdir -p $WORKDIR
+fi
+
 cd $WORKDIR
 
 # Clone some sample repos for playing with kubernetes and ansible
-git clone https://github.com/silvam11/ngci-demo.git
-git clone https://github.com/kubernetes-for-developers/kfd-flask.git
-git clone https://github.com/kubernetes-for-developers/kfd-nodejs.git
-#git clone https://github.com/kubernetes-for-developers/kfd-celery.git
-git clone https://github.com/spurin/masteringansible.git
+if [ ! -d "ngci-demo" ]; then
+    git clone https://github.com/silvam11/ngci-demo.git
+fi
+
+if [ ! -d "kfd-flask" ]; then
+    git clone https://github.com/kubernetes-for-developers/kfd-flask.git
+fi
+
+if [ ! -d "kfd-nodejs" ]; then
+    git clone https://github.com/kubernetes-for-developers/kfd-nodejs.git
+fi
+
+if [ ! -d "kfd-celery" ]; then
+    git clone https://github.com/kubernetes-for-developers/kfd-celery.git
+fi
+
+if [ ! -d "masteringansible" ]; then
+    git clone https://github.com/spurin/masteringansible.git
+fi
 
 # Use minikubes docker
 eval $(minikube docker-env)
