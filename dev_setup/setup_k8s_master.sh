64250ce5a507e4b59e15fd157db9078147f553f8
Kieran Murphy <Kieran.Murphy1@vce.com>
2018-06-09 22:03:00 +0100

    Updated k8s files

--- dev_setup/setup_k8s_master.sh
+++ dev_setup/setup_k8s_master.sh
@@ -5,9 +5,11 @@ source ./setup_prereqs
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Swap must be turned off
+sudo swapoff -a
 
 # Install kubernetes
 sudo apt-get update
@@ -28,4 +30,4 @@ kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documen
 kubectl apply -f https://raw.githubusercontent.com/coreos/flannel/master/Documentation/k8s-manifests/kube-flannel-rbac.yml
 
 # Check that the pod network has been deployed
-kubectl get pods —all-namespaces
+kubectl get pods --all-namespaces
--- dev_setup/setup_k8s_worker.sh
+++ dev_setup/setup_k8s_worker.sh
@@ -5,9 +5,11 @@ source ./setup_prereqs
 
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 
-sudo cat <<EOF > /etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
+sudo touch /etc/apt/sources.list.d/kubernetes.list
+echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+# Swap must be turned off
+sudo swapoff -a
 
 # Install kubernetes
 sudo apt-get update
@@ -19,3 +21,6 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config | tee -a ~/.bashrc
+
+# If all went well can join a k8s cluster with a command like the following:
+#kubeadm join 192.168.217.145:6443 --token uwtldh.147n0kr1t5cr8ujk --discovery-token-ca-cert-hash sha256:941c83742e2a9db1151e268aaa10f448ea40d9ec93ed581d1f1031fca0642b51
