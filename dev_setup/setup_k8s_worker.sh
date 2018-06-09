9b9f9207aee29955cbd9a78c278f6167a2eec640
Kieran Murphy <Kieran.Murphy1@vce.com>
2018-06-10 00:29:42 +0100

    Added domain and removed from preseed

--- dev_setup/setup_k8s_worker.sh
+++ dev_setup/setup_k8s_worker.sh
@@ -23,4 +23,4 @@ export KUBECONFIG=$HOME/.kube/config
 export KUBECONFIG=$HOME/.kube/config | tee -a ~/.bashrc
 
 # If all went well can join a k8s cluster with a command like the following:
-#kubeadm join 192.168.217.145:6443 --token uwtldh.147n0kr1t5cr8ujk --discovery-token-ca-cert-hash sha256:941c83742e2a9db1151e268aaa10f448ea40d9ec93ed581d1f1031fca0642b51
+#sudo kubeadm join 192.168.217.145:6443 --token uwtldh.147n0kr1t5cr8ujk --discovery-token-ca-cert-hash sha256:941c83742e2a9db1151e268aaa10f448ea40d9ec93ed581d1f1031fca0642b51
