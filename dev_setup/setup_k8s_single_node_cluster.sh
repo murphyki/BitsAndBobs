179012c24e7d93650544479fbb9a5358b63fba35
Kieran Murphy <kierantmurphy@gmail.com>
2018-06-11 15:33:51 +0100

    Update setup_k8s_single_node_cluster.sh

--- dev_setup/setup_k8s_single_node_cluster.sh
+++ dev_setup/setup_k8s_single_node_cluster.sh
@@ -6,4 +6,4 @@ source ./setup_k8s_master.sh
 # Allow pods to be scheuled on the master node, which
 # effectively turns this into a single node cluster.
 # See https://kubernetes.io/docs/setup/independent/create-cluster-kubeadm/
-kubectl taint nodes --all node-role.kubernetes.io/master-
+kubectl taint nodes --all node-role.kubernetes.io/master:NoSchedule-
